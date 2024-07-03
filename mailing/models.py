5963c4e30dede7ea4e6cead9ad5acde3c980f44a
menosad <draignair@gmail.com>
2024-07-03 18:51:57 +0300

    added model User first migrate

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    nickname = models.CharField(max_length=150, verbose_name='никнэйм', unique=True)
+    label = models.ImageField(upload_to='labels', **NULLABLE, verbose_name='лэйбел')
