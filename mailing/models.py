5426587cac1f059aa2b86c981f5bd51d8c4198a1
menosad <draignair@gmail.com>
2024-07-03 19:05:25 +0300

    rework model User

--- mailing/models.py
+++ mailing/models.py
@@ -7,3 +7,12 @@ class User(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     nickname = models.CharField(max_length=150, verbose_name='никнэйм', unique=True)
     label = models.ImageField(upload_to='labels', **NULLABLE, verbose_name='лэйбел')
+    created_at = models.DateTimeField(auto_created=True, auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}({self.nickname})"
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ('created_at',)
