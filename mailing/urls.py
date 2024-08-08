b9a769696187ec621e1fc28c0800bb0ff263849b
menosad <draignair@gmail.com>
2024-08-08 06:45:18 +0300

    CRUD сделал, теперь нужно прикрутить автоматическую рассылку

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import add_mailing, MailingListView, CreateMailing
+from mailing.views import add_mailing, MailingListView, MailingDetailView, update_mailing, MailingDelete
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', MailingListView.as_view(), name='main'),
+    path('mailing-detail/<int:pk>/', MailingDetailView.as_view(), name='detail'),
+    path('mailing-edit/<int:pk>/', update_mailing, name='edit'),
+    path('mailing-delete/<int:pk>/', MailingDelete.as_view(), name='delete'),
     path('add-mailing', add_mailing, name='add_mailing'),
-    path('datetimepicker/', add_mailing, name='picker'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,9 +1,27 @@
 from django.forms import formset_factory
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView, TemplateView
 
 from mailing.models import User, Mailing
+
+
+def edit_mailing(request, *args, **kwargs):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        departure_date = request.POST.get('departure_date').replace('/', '-', 3)
+        at_work = request.POST.get('at_work')
+        periodicity = request.POST.get('periodicity')
+        user = request.user
+        print(f'departure_date={departure_date, type(departure_date)}, periodicity={periodicity, type(periodicity)}')
+        mailing = Mailing.objects.create(title=title, content=content, departure_date=departure_date,
+                                         at_work=at_work,
+                                         periodicity=periodicity,
+                                         user=request.user)
+        mailing.save()
+        return mailing
 
 
 class MailingListView(ListView):
@@ -11,11 +29,36 @@ class MailingListView(ListView):
     model = Mailing
     template_name = 'mailing/index.html'
 
+    def get_queryset(self):
+        list_object = super().get_queryset()
+        if isinstance(self.request.user, int):
+            new_list = list_object.filter(user=self.request.user)
+            return new_list
+        else:
+            return list_object
 
-class CreateMailing(CreateView):
-    extra_context = {'title': 'список рассылок'}
+
+class MailingDetailView(DetailView):
     model = Mailing
-    success_url = reverse_lazy('mailing:index.html')
+
+
+class MailingDelete(DeleteView):
+    model = Mailing
+    success_url = reverse_lazy('mailing:main')
+
+
+def update_mailing(request, pk):
+    obj = Mailing.objects.get(pk=pk)
+    if request.method == 'POST':
+        obj.title = request.POST.get('title')
+        obj.content = request.POST.get('content')
+        obj.departure_date = request.POST.get('departure_date').replace('/', '-', 3)
+        obj.at_work = request.POST.get('at_work')
+        obj.periodicity = request.POST.get('periodicity')
+        obj.save()
+        request.path = f'mailing-detail/{obj.pk}'
+        return redirect(to=f'/')
+    return render(request, 'mailing/mailing_create.html')
 
 
 def add_mailing(request):
